0e642e019ef457cb908236d83de1be43bef697a8
MawunyoKwamiSedzro <msksedzro@gmail.com>
2026-09-24 07:48:53 +0000

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+// IGNIS School - Supabase Config
+const SUPABASE_CONFIG = {
+  url: "https://ovkkxygptieaskiewhcx.supabase.co",
+  anonKey: "sb_publishable_jp7TwWUCTNMUqVUjsFbYHg_OauxBFWa"
+};
+window.SUPABASE_CONFIG = SUPABASE_CONFIG;
