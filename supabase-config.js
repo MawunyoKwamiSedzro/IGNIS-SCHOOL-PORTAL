8b70b37052fb4cf73b63fa46382efb0a422d6c93
MawunyoKwamiSedzro <msksedzro@gmail.com>
2026-09-24 07:39:57 +0000

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,5 +0,0 @@
-const SUPABASE_CONFIG = {
-  url: "https://YOUR_PROJECT_ID.supabase.co",
-  anonKey: "eyJhbG... your REAL publishable key from Supabase > Project Settings > API"
-};
-window.SUPABASE_CONFIG = SUPABASE_CONFIG;
